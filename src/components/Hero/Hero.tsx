01becf8f2dce9806a91440a891209e9925990d71
Victor Moura <88560330+vomoura@users.noreply.github.com>
2025-05-28 17:18:32 +0000

    Updates

--- src/components/Hero/Hero.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { HeroWrapper } from './Hero.styles'
-import scooter from '../../../src/scooter.png'
-
-export const Hero: React.FC = () => (
-  <HeroWrapper>
-    <img src={scooter} alt="Scooter" />
-  </HeroWrapper>
-)
