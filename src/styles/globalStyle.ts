7d07348b2fbafc8dbf0dd4183b7e1746b305e155
Victor Moura <88560330+vomoura@users.noreply.github.com>
2025-06-24 15:36:50 -0300

    Remove font import from global styles

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,17 +2,15 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  /* 1) importa a fonte do Google */
-  @import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;600;700&display=swap');
 
-  /* 2) reset básico */
+  /* reset básico */
   *, *::before, *::after {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
   }
 
-  /* 3) estilos globais */
+  /* estilos globais */
   body {
     font-family: ${({ theme }) => theme.fonts.primary};
     background: ${({ theme }) => theme.colors.background};
